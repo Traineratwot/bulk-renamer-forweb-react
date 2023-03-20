500fae4c88d0effb981336241b4e21fdc23153b3
Aidan647 <kirillkotka@gmail.com>
2023-03-20 18:39:12 +0200

    format

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-app.use(bodyParser.json({ type: 'application/*+json' }))
+const express = require("express")
+const bodyParser = require("body-parser")
+const path = require("path")
+const app = express()
+app.use(express.static(path.join(__dirname, "build")))
+app.use(bodyParser.json({ type: "application/*+json" }))
 
-app.get('/ping', function (req, res) {
-	return res.send('pong');
-});
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.get("/ping", function (req, res) {
+	return res.send("pong")
+})
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "build", "index.html"))
+})
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080)
